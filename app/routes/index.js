d72118af0db5f5a2ae3cc345f0130171b339d32a
Simon Whatley <23444+simonwhatley@users.noreply.github.com>
2020-06-11 17:12:16 +0100

    Add URL preview component (#107)

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,4 +9,12 @@ router.get('/components', function(req, res) {
   res.redirect('/');
 });
 
+router.get('/generate-url', function(req, res) {
+  let slug = req.query.title.toLowerCase()
+                    .replace(/[^\w\s-]/g, '') // remove non-word [a-z0-9_], non-whitespace, non-hyphen characters
+                    .replace(/[\s_-]+/g, '-') // swap any length of whitespace, underscore, hyphen characters with a single -
+                    .replace(/^-+|-+$/g, '')
+  res.send(slug);
+});
+
 module.exports = router;
--- src/govuk-pub/helpers.js
+++ src/govuk-pub/helpers.js
@@ -3,11 +3,11 @@
  * This seems to fail in IE8, requires more investigation.
  * See: https://github.com/imagitama/nodelist-foreach-polyfill
  */
-export function nodeListForEach (nodes, callback) {
-  if (window.NodeList.prototype.forEach) {
-    return nodes.forEach(callback)
-  }
-  for (var i = 0; i < nodes.length; i++) {
-    callback.call(window, nodes[i], i, nodes)
-  }
-}
+// export function nodeListForEach (nodes, callback) {
+//   if (window.NodeList.prototype.forEach) {
+//     return nodes.forEach(callback)
+//   }
+//   for (var i = 0; i < nodes.length; i++) {
+//     callback.call(window, nodes[i], i, nodes)
+//   }
+// }
