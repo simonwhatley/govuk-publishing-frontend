144914ff59b46a0858e69adaf6e9b2e2f68bb212
Simon Whatley <23444+simonwhatley@users.noreply.github.com>
2020-06-15 15:02:42 +0100

    Add cookie banner component (#119)

---
+++ src/govuk-pub/components/cookie-banner/README.md
@@ -0,0 +1,40 @@
+# Cookie banner
+
+Help users manage their personal data by telling them when you store cookies on their device.
+
+[Preview the component](https://govuk-publishing-frontend.herokuapp.com/components/cookie-banner/)
+
+## Example usage
+
+```
+{{ govukPubCookieBanner({
+  
+}) }}
+```
+
+## Accessibility acceptance criteria
+
+Text in the cookie banner must be clear and unambiguous and should provide a way to dismiss the message.
+
+Links in the component must:
+
+- accept focus
+- be focusable with a keyboard
+- be usable with a keyboard
+- indicate when they have focus
+- change in appearance when touched (in the touch-down state)
+- change in appearance when hovered
+- be usable with touch
+- be usable with [voice commands](https://www.w3.org/WAI/perspectives/voice.html)
+- have visible text
+- have meaningful text
+
+## Arguments
+
+This component accepts the following arguments.
+
+|Name|Type|Required|Description|
+|---|---|---|---|
+
+
+*Warning: If you’re using Nunjucks macros in production be aware that using HTML arguments, or ones ending with `.html` can be at risk from [cross-site scripting](https://en.wikipedia.org/wiki/Cross-site_scripting) attacks. More information about security vulnerabilities can be found in the [Nunjucks documentation](https://mozilla.github.io/nunjucks/api.html#user-defined-templates-warning).*
---
+++ src/govuk-pub/components/cookie-banner/cookie-banner.js
@@ -0,0 +1,101 @@
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {};
+
+(function (Modules) {
+  function CookieBanner () { }
+
+  CookieBanner.prototype.start = function ($module) {
+    this.$module = $module[0]
+    this.$module.hideCookieMessage = this.hideCookieMessage.bind(this)
+    this.$module.showConfirmationMessage = this.showConfirmationMessage.bind(this)
+    this.$module.setCookieConsent = this.setCookieConsent.bind(this)
+
+    this.$module.cookieBanner = document.querySelector('.govuk-pub-cookie-banner')
+    this.$module.cookieBannerConfirmationMessage = this.$module.querySelector('.govuk-pub-cookie-banner__confirmation')
+
+    this.setupCookieMessage()
+  }
+
+  CookieBanner.prototype.setupCookieMessage = function () {
+    this.$hideLinks = this.$module.querySelectorAll('button[data-hide-cookie-banner]')
+    if (this.$hideLinks && this.$hideLinks.length) {
+      for (var i = 0; i < this.$hideLinks.length; i++) {
+        this.$hideLinks[i].addEventListener('click', this.$module.hideCookieMessage)
+      }
+    }
+
+    this.$acceptCookiesLink = this.$module.querySelector('button[data-accept-cookies]')
+    if (this.$acceptCookiesLink) {
+      this.$acceptCookiesLink.addEventListener('click', this.$module.setCookieConsent)
+    }
+
+    this.showCookieMessage()
+  }
+
+  CookieBanner.prototype.showCookieMessage = function () {
+    // Show the cookie banner if not in the cookie settings page or in an iframe
+    if (!this.isInCookiesPage() && !this.isInIframe()) {
+      var shouldHaveCookieMessage = (this.$module && window.GOVUK.cookie('cookies_preferences_set') !== 'true')
+
+      if (shouldHaveCookieMessage) {
+        this.$module.style.display = 'block'
+
+        // Set the default consent cookie if it isn't already present
+        if (!window.GOVUK.cookie('cookies_policy')) {
+          window.GOVUK.setDefaultConsentCookie()
+        }
+
+        window.GOVUK.deleteUnconsentedCookies()
+      } else {
+        this.$module.style.display = 'none'
+      }
+    } else {
+      this.$module.style.display = 'none'
+    }
+  }
+
+  CookieBanner.prototype.hideCookieMessage = function (event) {
+    if (this.$module) {
+      this.$module.style.display = 'none'
+      window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
+    }
+
+    if (event.target) {
+      event.preventDefault()
+    }
+  }
+
+  CookieBanner.prototype.setCookieConsent = function () {
+    window.GOVUK.approveAllCookieTypes()
+    this.$module.showConfirmationMessage()
+    this.$module.cookieBannerConfirmationMessage.focus()
+    window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
+    if (window.GOVUK.analyticsInit) {
+      window.GOVUK.analyticsInit()
+    }
+    if (window.GOVUK.globalBarInit) {
+      window.GOVUK.globalBarInit.init()
+    }
+  }
+
+  CookieBanner.prototype.showConfirmationMessage = function () {
+    this.$cookieBannerMainContent = document.querySelector('.govuk-pub-cookie-banner__wrapper')
+
+    this.$cookieBannerMainContent.style.display = 'none'
+    this.$module.cookieBannerConfirmationMessage.style.display = 'block'
+  }
+
+  CookieBanner.prototype.listenForCrossOriginMessages = function () {
+    window.addEventListener('message', this.receiveMessage.bind(this), false)
+  }
+
+  CookieBanner.prototype.isInCookiesPage = function () {
+    return window.location.pathname === '/help/cookies'
+  }
+
+  CookieBanner.prototype.isInIframe = function () {
+    return window.parent && window.location !== window.parent.location
+  }
+
+  Modules.CookieBanner = CookieBanner
+})(window.GOVUK.Modules)
