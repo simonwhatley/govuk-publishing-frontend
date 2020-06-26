3e3a711e9c456981887e133061a8e96d53fa7f94
Simon Whatley <23444+simonwhatley@users.noreply.github.com>
2020-06-26 18:01:16 +0100

    Add chevron banner component (#140)

--- gulp/copy-assets.js
+++ gulp/copy-assets.js
@@ -3,7 +3,8 @@ const gulp = require('gulp');
 gulp.task('copy-assets', () => {
   return gulp.src([
       '!app/assets/sass{,/**/*}',
-      'app/assets/**'
+      'app/assets/**',
+      'src/govuk-pub/assets/**'
     ])
     .pipe(gulp.dest('public/'));
 });
