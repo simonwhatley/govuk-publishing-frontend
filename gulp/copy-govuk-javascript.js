a8bb763cddd5b568b1f2252d8f707742ad8b0400
Simon Whatley <simonwhatley@gmail.com>
2020-07-02 22:07:51 +0100

    Fix page preview tabs

---
+++ gulp/copy-govuk-javascript.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+
+gulp.task('copy-govuk-javascript', () => {
+  return gulp.src([
+      'node_modules/govuk-frontend/govuk/all.js'
+    ])
+    .pipe(gulp.dest('public/javascripts/govuk'));
+});
